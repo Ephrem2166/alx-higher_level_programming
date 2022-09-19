9c7561f61981fc39501e3d0974706c1af5d74819
Ephrem2166 <ephrem2181@gmail.com>
2022-09-19 03:40:13 -0700

    initial solutions

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""Defines a print_square function."""
+
+import string
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of these characters: ., ? and :
+    Args:
+        text(str): the string to print.
+    Raises:
+        TypeError exception: If the text is not string.
+    """
+    if not isinstance(text, str): 
+        raise TypeError ("text must be a string")
+
+    i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
+
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == '\n' or text[i] in ".?:":
+            if text[i] in ".?:":
+                print("\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
+        if i < len(text)-1:
+            if text[i] == ' ' and text[i+1] == '\n':
+                i += 1
